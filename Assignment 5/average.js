c38745df27c15603292060814f5e363596d43dcb
curbaniATU <curbani@atu.edu>
2025-01-31 12:42:06 -0600

    Assignment 5: Wrote an average function in javascript.

---
+++ Assignment 5/average.js
@@ -0,0 +1,14 @@
+// Average Function
+function average(int1, int2) {
+    comboInt = int1 + int2;
+    avgInt = comboInt / 2;
+    return avgInt;
+}
+
+// Variable Declaration
+int1 = 7;
+int2 = 9;
+
+avg = average(int1, int2);
+
+console.log(avg);
