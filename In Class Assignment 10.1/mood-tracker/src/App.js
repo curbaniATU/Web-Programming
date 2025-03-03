aaae6e89e2ba820080f152fd0c487dc1a2bfa0bc
curbaniATU <curbani@atu.edu>
2025-03-03 13:26:06 -0600

    In Class Assignment 10.1: Created Mood Tracker App

---
+++ In Class Assignment 10.1/mood-tracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MoodTracker from './MoodTracker';
+
+function App() {
+  return (
+    <div>
+      <MoodTracker />
+    </div>
+  );
+}
+
+export default App;
